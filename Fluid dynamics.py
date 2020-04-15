7dbf7ad1ebad0d77e5a93ee22e649060985440a3
RosePablo <63450182+RosePablo@users.noreply.github.com>
2020-04-15 20:42:16 +0100

    Add files via upload

--- Fluid dynamics.py
+++ Fluid dynamics.py
@@ -8,7 +8,23 @@ w=[]
 z=[]
 xpx=[]
 ypx=[]
-t=[]
+t_hour=[]
+t_min=[]
+t_sec=[]
+t_msec=[]
+
+#Reading rpm txt for time
+with open("20130320_Track_17_5_rpm.txt","r+") as f:
+    data=f.readlines()
+    for line in data:
+        t_hour.append(line.strip().split(",")[0])
+    for line in data:
+        t_min.append(line.strip().split(",")[1])
+    for line in data:
+        t_sec.append(line.strip().split(",")[2])
+    for line in data:
+        t_msec.append(line.strip().split(",")[3])
+
 
 #Reading windspeed csv
 with open('radius_windspeed.csv') as csvfile:
@@ -24,23 +40,16 @@ with open("20130320_Track_17_5_rpm.txt","r+") as f:
         xpx.append(line.strip().split(",")[4])
     for line in data:
         ypx.append(line.strip().split(",")[5])
-    for line in data:
-        t.append(line.strip().split(",")[2:4])
 
 #Removing header from lists
 xpx=xpx[1:]
 ypx=ypx[1:]
 w=w[1:]
 z=z[1:]
-t=t[1:]
-
-#Changing time to more reasonable format
-for i in range(0, len(t)):
-    t[i]=','.join(t[i])
-for i in range(0, len(t)):
-    t[i]=t[i].replace(",","")
-for i in range(0, len(t)):
-    t[i]=t[i].replace("0","",1)
+t_hour=t_hour[1:]
+t_min=t_min[1:]
+t_sec=t_sec[1:]
+t_msec=t_msec[1:]
 
 #Changing string to int
 def int_loop(Z):
@@ -48,7 +57,6 @@ def int_loop(Z):
     Z[i]=int(Z[i])
   return Z
 
-int_loop(t)
 int_loop(xpx)
 int_loop(ypx)
 
@@ -60,13 +68,25 @@ def float_loop(Z):
 
 float_loop(w)
 float_loop(z)
+float_loop(t_hour)
+float_loop(t_min)
+float_loop(t_sec)
+float_loop(t_msec)
 
 #Changing lists to arrays
 xpxa=np.array(xpx)
 ypxa=np.array(ypx)
 wa=np.array(w)
 za=np.array(z)
-ta=np.array(t)/1000
+
+
+ta_hour=np.array(t_hour)
+ta_min=np.array(t_min)
+ta_sec=np.array(t_sec)
+ta_msec=np.array(t_msec)
+
+#Converting to time
+ta= ta_hour*3600 + ta_min*60 + ta_sec + ta_msec/1000
 
 #Points
 X=xpxa-206
@@ -96,14 +116,14 @@ for i in range(len(diff_theta)):
 ur=diff_r/diff_t
 utheta=diff_theta/diff_t
 
-#R/Rmax and omega
+#R/Rmax and omega converting to rad s-1
 k=r/rmax
-omega=17.5
+omega=17.5*0.1047
 
 #Removing last point from array
 new_r=np.delete(r,218)
 new_k=np.delete(k,218)
-new_t=np.delete(t,218)
+new_t=np.delete(ta,218)
 
 #Rossby number
 Ro=utheta/(2*omega*new_r)
@@ -135,13 +155,29 @@ ang_mom=U_theta*new_r
 theory=1/2*((np.square(rmax))/(np.square(new_r))-1)
 
 #Plotting functions
-plt.plot(x,y)
+plt.plot(x,y,'r')
+plt.title('Trajectory of black paper dot')
 plt.show()
-plt.plot(new_k,Ro)
+plt.plot(new_k,Ro,'r')
+plt.title('Rossby number over r/rmax (wet experiment)')
 plt.show()
-plt.plot(new_t,ang_mom)
+plt.plot(new_t,ang_mom,'r')
+plt.title('Angular momentum over time')
 plt.show()
-plt.plot(new_k,theory)
+plt.plot(new_k,theory,'r')
+plt.title('Theoretical Rossby prediction')
 plt.show()
-plt.plot(o,rossby)
+plt.plot(o,rossby,'r')
+plt.title('Rossby number over r/rmax (Dry experiment)')
+plt.show()
+
+plt.subplot(3,1,1)
+plt.plot(new_k,Ro,'r')
+plt.title('Rossby number over r/rmax (wet experiment)')
+plt.subplot(3,1,2)
+plt.plot(o,rossby,'r')
+plt.title('Rossby number over r/rmax (Dry experiment)')
+plt.subplot(3,1,3)
+plt.plot(new_k,theory,'r')
+plt.title('Theoretical Rossby prediction')
 plt.show()
